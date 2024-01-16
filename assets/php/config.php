86681dbaa1dacb8a7a57cc72731687a36e0bbeb4
Pascal.Jura <104727003+pascaljura@users.noreply.github.com>
2024-01-16 17:01:28 +0100

    Login form, style, structure edit

---
+++ assets/php/config.php
@@ -0,0 +1,19 @@
+<?php
+// Přístupové údaje k databázi (Localhost)
+$servername = "localhost";
+$username = 'root';
+$password = "";
+$dbname = "parla";
+
+// Přístupové údaje k databázi
+//$servername = "md75.wedos.net";
+//$username = 'a237642_m';
+//$password = "XxgDSx2L";
+//$dbname = "d237642_m";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+$conn->set_charset("utf8");
+if ($conn->connect_error) {
+    die("Chyba při připojování k databázi: " . $conn->connect_error);
+}
+?>
