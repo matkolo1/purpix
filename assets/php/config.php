a5b09b79b4f0f360bdd7420985622525cd96223a
matkolo1 <142239358+matkolo1@users.noreply.github.com>
2024-01-18 16:45:55 +0100

    zde

--- assets/php/config.php
+++ assets/php/config.php
@@ -1,15 +1,15 @@
 <?php
 // Přístupové údaje k databázi (Localhost)
-//$servername = "localhost";
-//$username = 'root';
-//$password = "";
-//$dbname = "parla";
+$servername = "localhost";
+$username = 'root';
+$password = "";
+$dbname = "parla";
 
 // Přístupové údaje k databázi
-$servername = "md75.wedos.net";
-$username = 'a237642_m';
-$password = "XxgDSx2L";
-$dbname = "d237642_m";
+//$servername = "md75.wedos.net";
+//$username = 'a237642_m';
+//$password = "XxgDSx2L";
+//$dbname = "d237642_m";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 $conn->set_charset("utf8");
--- lvl1.php
+++ lvl1.php
@@ -2,33 +2,34 @@
 include './assets/php/config.php';
 session_start();
 if (!isset($_SESSION['user_id'])) {
-    header("Location: ./index.php");
-    exit();
+	header("Location: ./index.php");
+	exit();
 }
 if (isset($_POST['logout'])) {
-    session_unset();
-    session_destroy();
-    header("Location: ./index.php");
-    exit();
+	session_unset();
+	session_destroy();
+	header("Location: ./index.php");
+	exit();
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./assets/css/lvl_style.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="./assets/css/lvl_style.css">
 </head>
 
 <body>
-    <form method="post">
-        <button type="submit" name="logout">Odhlásit
-            se</button>
-    </form>
-    <canvas id="gameCanvas"></canvas>
+	<canvas id="gameCanvas"></canvas>
+	<form method="post">
+		<button type="submit" name="logout">Odhlásit
+			se</button>
+	</form>
 
-    <script src="./assets/js/lvl1_script.js"></script>
+	<script src="./assets/js/lvl1_script.js"></script>
+
 
 </body>
 
