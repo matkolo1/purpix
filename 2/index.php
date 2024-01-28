7e5cb0b4ffddc79f924234d23cb1bd030c6cb2af
Pascal.Jura <104727003+pascaljura@users.noreply.github.com>
2024-01-28 14:17:06 +0100

    Title and icon added

--- 2/index.php
+++ 2/index.php
@@ -41,6 +41,9 @@ if ($urlNumber > 0) {
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="./assets/style.css">
+	<link rel="icon" href="../assets/images/icon.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/images/icon.png" type="image/x-icon">
+    <title>PurPix</title>
 </head>
 
 <body>
